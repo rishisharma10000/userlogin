175a095f1bc31113ecad8b8fb4fcb7b71827656e
Rishi Sharma <rishisharma@Rishis-MacBook-Pro.local>
2018-05-30 17:12:21 +0100

    pushing on github

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,5 @@
 import firebase from "firebase";
-
-const config = {
-  apiKey: "AIzaSyARQzQEV6YkxNKQuT5i10Mt8G8LoeJxUCM",
-  authDomain: "users-5dc8a.firebaseapp.com",
-  databaseURL: "https://users-5dc8a.firebaseio.com",
-  projectId: "users-5dc8a",
-  storageBucket: "users-5dc8a.appspot.com",
-  messagingSenderId: "429924995962"
-};
+import config from "./config";
 firebase.initializeApp(config);
 
 export default firebase;
